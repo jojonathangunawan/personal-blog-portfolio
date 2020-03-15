0c56a22020c37459c88f4ca73c2fb4c24ac90c25
jojonathangunawan <jojonathangunawan@gmail.com>
2020-03-15 23:35:12 +0700

    get homepage data from API

--- frontend/src/containers/Homepage.vue
+++ frontend/src/containers/Homepage.vue
@@ -1,9 +1,49 @@
 <template>
-  <h1>HELLOOO INI HALAMAN HOMEPAGE</h1>
+  <div>
+    <div class="uk-text-center">
+      <h1>{{homepage.title}}</h1>
+      <p>{{homepage.description}}</p>
+    </div>
+    <div class="uk-grid-column-small uk-child-width-expand@s uk-text-center grid-container" uk-grid>
+      <div class="grid-img">
+        <div class="grid-img-box" v-for="banner in homepage.banners" :key="banner.id">
+            <img class="img-banner" :src= "api_url + banner.url" :alt="banner.name">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script src="./js/homepage.js"></script>
 
-<style scoped>
+<style lang="css" scoped>
+
+  .description {
+    text-align: center;
+  }
+
+  .grid-container{
+    margin: 50px 0;
+  }
+
+  .grid-img{
+    /*border: 1px solid blue;*/
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .grid-img-box{
+    /*border: 1px solid red;*/
+    width: 450px;
+    height: auto;
+    margin-right: 15px;
+  }
+
+  .img-banner{
+    max-width: 100%;
+    max-height: 100%;
+    border-radius: 12px;
+  }
 
 </style>
--- frontend/src/containers/js/homepage.js
+++ frontend/src/containers/js/homepage.js
@@ -1,3 +1,25 @@
+import gql from "graphql-tag/src";
+
 export default {
-  name: "Homepage.vue"
+  name: "Homepage.vue",
+  data() {
+    return {
+      homepage: {},
+      api_url: process.env.VUE_APP_STRAPI_API_URL
+    }
+  },
+  apollo: {
+    homepage: gql`
+    query Homepage {
+      homepage {
+        title
+        description
+        banners{
+          id
+          url
+          name
+        }
+      }
+    }`
+  }
 }
